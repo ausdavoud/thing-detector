52329827c225a7ba258978ca124d8257e8a7a770
Eli <eli2002ch@gmail.com>
2025-12-06 22:52:11 +0330

    webcam width changed

--- frontend/src/YOLOWebcam.jsx
+++ frontend/src/YOLOWebcam.jsx
@@ -70,7 +70,7 @@ export default function YOLOWebcam({ onDetections, onSnapshot }) {
   }
 
   return (
-    <div style={{ position: "relative" }}>
+    <div style={{ position: "relative", width: "100%"}}>
       <video ref={videoRef} style={{ width: "100%" }} />
       <canvas
         ref={canvasRef}
